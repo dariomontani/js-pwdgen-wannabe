a504e37f614c0f7ee1ecdc6a598d1fa82422bdfc
dariomontani <dariomontani@gmail.com>
2021-11-18 15:16:54 +0100

    add nome js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Descrizione esercizio:
+// 1. Chiedi all’utente il suo nome,
+// 2. poi chiedi il suo cognome,
+// 3. poi chiedi il suo colore preferito
+// 4. Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+// chiedo all'utente il suo nome
+let nomeUtente = prompt('Inserisci il tuo Nome');
+console.log(nomeUtente)
